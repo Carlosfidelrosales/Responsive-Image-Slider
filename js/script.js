3dd5ebf6c84838a98b8c20a04bf5ed65b8a6c5b2
Pidel <fidelcarlos302@gmail.com>
2023-08-16 14:19:49 +0800

    Updated the layout and fix the JavaScript error. Perfectly working and responsive.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,331 @@
-var swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
+// var swiper = new Swiper(".slide-content", {
+//     slidesPerView: 1,
+//     spaceBetween: 25,
+//     loop: true,
+//     centerSlide: 'true',
+//     fade: 'true',
+//     grabCursor: 'true',
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//       dynamicBullets: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+
+//     breakpoints:{
+//         0: {
+//             slidesPerView: 1,
+//         },
+//         520: {
+//             slidesPerView: 2,
+//         },
+//         950: {
+//             slidesPerView: 3,
+//         },
+//     },
+//   });
+
+
+
+//   var swiperConfig = {
+//     slidesPerView: numVisibleCards,
+//     spaceBetween: 25,
+//     loop: true,
+//     centerSlide: true,
+//     fade: true,
+//     grabCursor: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//       dynamicBullets: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   };
+
+//   if (numVisibleCards === 1) {
+//     swiperConfig.loop = false; // Disable looping
+//     swiperConfig.centeredSlides = false; // Disable centering
+//   }
+
+//   swiper = new Swiper(".slide-content", swiperConfig);
+// }
+
+// // Initialize the slider on page load
+// initializeSlider();
+
+// // Check and reinitialize the slider on window resize
+// window.addEventListener("resize", function () {
+//   swiper.destroy(); // Destroy the existing slider
+//   initializeSlider(); // Initialize a new slider
+// });
+
+
+// function initializeSlider() {
+//   var screenWidth = window.innerWidth;
+
+//   if (screenWidth >= 768) {
+//     var cardContainers = document.querySelectorAll(".card");
+//     var numVisibleCards = Math.min(3, cardContainers.length);
+
+//     var swiperConfig = {
+//       slidesPerView: numVisibleCards,
+//       spaceBetween: 25,
+//       loop: true,
+//       centerSlide: true,
+//       fade: true,
+//       grabCursor: true,
+//       pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//         dynamicBullets: true,
+//       },
+//       navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//       },
+//     };
+
+//     if (numVisibleCards === 1) {
+//       swiperConfig.loop = false; // Disable looping
+//       swiperConfig.centeredSlides = false; // Disable centering
+//     }
+
+//     var swiper = new Swiper(".slide-content", swiperConfig);
+//   }
+// }
+
+// // Initialize the slider on page load
+// initializeSlider();
+
+// // Check and reinitialize the slider on window resize
+// window.addEventListener("resize", initializeSlider);
+
+
+
+// function initializeSlider() {
+//   var screenWidth = window.innerWidth;
+
+//   if (screenWidth >= 768) {
+//     var cardContainers = document.querySelectorAll(".card");
+//     var numVisibleCards = Math.min(3, cardContainers.length);
+
+//     var swiperConfig = {
+//       slidesPerView: numVisibleCards,
+//       spaceBetween: 25,
+//       loop: true,
+//       centerSlide: true,
+//       fade: true,
+//       grabCursor: true,
+//       pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//         dynamicBullets: true,
+//       },
+//       navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//       },
+//     };
+
+//     if (numVisibleCards === 1) {
+//       swiperConfig.loop = false; // Disable looping
+//       swiperConfig.centeredSlides = false; // Disable centering
+//     }
+
+//     var swiper = new Swiper(".slide-content", swiperConfig);
+//   }
+// }
+
+// // Initialize the slider on page load
+// initializeSlider();
+
+// // Check and reinitialize the slider on window resize
+// window.addEventListener("resize", initializeSlider);
+
+
+
+// function updateSwiperBreakpoints(numVisibleCards) {
+//   var breakpoints = {
+//     0: {
+//       slidesPerView: 1,
+//     },
+//   };
+
+//   if (numVisibleCards == 2) {
+//     breakpoints[520] = {
+//       slidesPerView: 2,
+//     };
+//   }
+
+//   if (numVisibleCards >= 3) {
+//     breakpoints[950] = {
+//       slidesPerView: 3,
+//     };
+//   }
+
+//   return breakpoints;
+// }
+
+// function initializeSlider() {
+//   var screenWidth = window.innerWidth;
+//   var cardContainers = document.querySelectorAll(".card");
+//   var numVisibleCards = Math.min(3, cardContainers.length);
+
+//   var swiperConfig = {
+//     slidesPerView: numVisibleCards,
+//     spaceBetween: 25,
+//     loop: true,
+//     centerSlide: true,
+//     fade: true,
+//     grabCursor: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//       dynamicBullets: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     breakpoints: updateSwiperBreakpoints(numVisibleCards), // Set breakpoints
+//   };
+
+//   if (numVisibleCards === 1) {
+//     swiperConfig.loop = false; // Disable looping
+//     swiperConfig.centeredSlides = false; // Disable centering
+//   }
+
+//   var swiper = new Swiper(".slide-content", swiperConfig);
+// }
+
+// // Initialize the slider on page load
+// initializeSlider();
+
+// // Check and reinitialize the slider on window resize
+// window.addEventListener("resize", function () {
+//   initializeSlider();
+// });
+
+
+// OKAY CODE
+// var swiper = null; // Declare the swiper variable globally
+
+// function updateSwiperBreakpoints(numVisibleCards) {
+//   var breakpoints = {
+//     0: {
+//       slidesPerView: 1,
+//     },
+//   };
+
+//   if (numVisibleCards == 2) {
+//     breakpoints[520] = {
+//       slidesPerView: 2,
+//     };
+//   }
+
+  // if (numVisibleCards >= 3) {
+  //   breakpoints[950] = {
+  //     slidesPerView: 3,
+  //   };
+  // }
+
+//   return breakpoints;
+// }
+
+// function initializeSlider() {
+//   var screenWidth = window.innerWidth;
+//   var cardContainers = document.querySelectorAll(".card");
+//   var numVisibleCards = Math.min(3, cardContainers.length);
+
+//   var swiperConfig = {
+//     slidesPerView: numVisibleCards,
+//     spaceBetween: 25,
+//     loop: true,
+//     centerSlide: true,
+//     fade: true,
+//     grabCursor: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//       dynamicBullets: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     breakpoints: updateSwiperBreakpoints(numVisibleCards), // Set breakpoints
+//   };
+
+//   if (numVisibleCards === 1) {
+//     swiperConfig.loop = false; // Disable looping
+//     swiperConfig.centeredSlides = false; // Disable centering
+//   }
+
+//   // Destroy the existing Swiper instance if it exists
+//   if (swiper) {
+//     swiper.destroy();
+//   }
+
+//   // Initialize a new Swiper instance
+//   swiper = new Swiper(".slide-content", swiperConfig);
+// }
+
+// // Initialize the slider on page load
+// initializeSlider();
+
+// // Check and reinitialize the slider on window resize
+// window.addEventListener("resize", function () {
+//   initializeSlider();
+// });
+
+
+var swiper = null; // Declare the swiper variable globally
+
+function updateNumVisibleCards() {
+  var screenWidth = window.innerWidth;
+  var cardContainers = document.querySelectorAll(".card");
+  return Math.min(3, cardContainers.length); // Limit to a maximum of 3 cards
+}
+
+function updateSwiperBreakpoints(numVisibleCards) {
+  var breakpoints = {
+    0: {
+      slidesPerView: 1,
+    },
+  };
+
+  if (numVisibleCards == 2) {
+    breakpoints[520] = {
+      slidesPerView: 2,
+    };
+  }
+
+  if (numVisibleCards == 3) {
+    breakpoints[950] = {
+      slidesPerView: 3,
+    };
+  }
+  
+
+  return breakpoints;
+}
+
+function initializeSlider() {
+  var numVisibleCards = updateNumVisibleCards();
+
+  var swiperConfig = {
+    slidesPerView: numVisibleCards,
     spaceBetween: 25,
     loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
+    centerSlide: true,
+    fade: true,
+    grabCursor: true,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -14,16 +335,37 @@ var swiper = new Swiper(".slide-content", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    breakpoints: updateSwiperBreakpoints(numVisibleCards), // Set breakpoints
+  };
 
-    breakpoints:{
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        },
-    },
-  });
+  if (numVisibleCards === 1) {
+    swiperConfig.loop = false; // Disable looping
+    swiperConfig.centeredSlides = false; // Disable centering
+  }
+  if (numVisibleCards === 2) {
+    swiperConfig.loop = false;
+    swiperConfig.centeredSlides = false;
+
+  }
+  if (numVisibleCards === 3) {
+    swiperConfig.loop = false;
+    swiperConfig.centeredSlides = false;
+
+  }
+
+  // Destroy the existing Swiper instance if it exists
+  if (swiper) {
+    swiper.destroy();
+  }
+
+  // Initialize a new Swiper instance
+  swiper = new Swiper(".slide-content", swiperConfig);
+}
+
+// Initialize the slider on page load
+initializeSlider();
+
+// Check and reinitialize the slider on window resize
+window.addEventListener("resize", function () {
+  initializeSlider();
+});
